e835eecb569ac9fe293344366c5b3facc2e370af
Jhon Navarrete <jhnavarrete@MacBook-Pro.local>
2023-06-18 02:13:51 -0500

    Include a specific font in the proyect.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ['index.html', './src/**/*.{js, jsx, ts, tsx}'],
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+},
   },
   plugins: [],
 }
